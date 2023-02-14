0978a42049ed23bb750f833b9b672aac9380e07a
shroom00 <124786038+shroom00@users.noreply.github.com>
2023-02-14 14:46:55 +0000

    Update exceptions.py
    Made code more readable.

--- exceptions.py
+++ exceptions.py
@@ -1,9 +1,4 @@
-class PinpyException(Exception):
-    """Base exception for Pinterest Errors"""
-    pass
-
-
-class PinterestError(PinpyException):
+class PinterestError:
     """Class to handle json formatted pinterest errors"""
 
     def __init__(self, response) -> None:
@@ -13,5 +8,11 @@ class PinterestError(PinpyException):
             self.response_json = self.response_json["body"]["response"]["error"]
         self.api_code = self.response_json["code"]
         self.api_message = self.response_json["message"]
-        self.message_detail = self.response_json["message_detail"] if "message_detail" in self.response_json else None
-        super().__init__(f"({self.api_code}) {self.api_message}{' - ' + self.message_detail if self.message_detail else ''}")
+        self.message_detail = (
+            self.response_json["message_detail"]
+            if "message_detail" in self.response_json
+            else None
+        )
+        super().__init__(
+            f"({self.api_code}) {self.api_message}{' - ' + self.message_detail if self.message_detail else ''}"
+        )
